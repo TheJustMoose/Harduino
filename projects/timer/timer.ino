a405098d8104b263bd57794c63e5b2497c37eb2f
TheJustMoose <vmorkva@ya.ru>
2020-11-01 23:56:44 +0300

    Timer. Initial commit.

---
+++ projects/timer/timer.ino
@@ -0,0 +1,149 @@
+#include <TimerOne.h>
+
+// hardware:
+// 3 tactile button + rotary encoder button
+// 74HC139 decoder
+
+// connections:
+// pin 12 == PB4 -> A0 pcb -> 13 (A1|B) decoder pin
+// pin 13 == PB5 -> A1 pcb -> 14 (A0|A) decoder pin
+// pin A2 == PC2 -> all tactile buttons + pullup
+// pin A1 == PC1 -> rotary encoder pin A
+// pin A0 == PC0 -> rotary encoder pin B
+// pin 8-11 == PB0-3 -> 74LS247 pins A-D
+// pin 2 -> decimal point
+
+// Do not use "const byte" here, it's not work
+// Input ports
+#define ENC_PORT PINC
+#define BTN_PORT PINC
+#define BTN_BIT  2
+
+// Output ports
+#define DIG_PORT PORTB
+#define DIG_BIT_B 4  // 74HC139
+#define DIG_BIT_A 5
+
+#define NUM_PORT PORTB
+#define NUM_BIT_0 0  // 74LS247
+#define NUM_BIT_1 1
+#define NUM_BIT_2 2
+#define NUM_BIT_3 3
+
+const int line = 4;
+
+const int dp = 2;  // decimal point
+
+const int d0 = 8;
+const int d1 = 9;
+const int d2 = 10;
+const int d3 = 11;
+
+const int a0 = 13;  // yes! it's fail :(
+const int a1 = 12;
+
+const int del = 5; // delay ms
+
+uint8_t btn_was_pressed[4] = {0, 0, 0, 0};
+uint8_t btn_pressed[4] = {0, 0, 0, 0};
+uint8_t LED[4] = {0, 1, 2, 3}; // the numbers on the LED
+uint8_t digit = 0; // current processed digit | button
+
+void set_addr(uint8_t addr) {
+  // PB5 == A decoder pin
+  if (addr & 0x01)
+    bitSet(DIG_PORT, DIG_BIT_A);
+  else
+    bitClear(DIG_PORT, DIG_BIT_A);
+
+  // PB4 == B decoder pin
+  if (addr & 0x02)
+    bitSet(DIG_PORT, DIG_BIT_B);
+  else
+    bitClear(DIG_PORT, DIG_BIT_B);
+}
+
+void set_number(uint8_t number) {
+  if (number & 0x01)
+    bitSet(NUM_PORT, NUM_BIT_0);
+  else
+    bitClear(NUM_PORT, NUM_BIT_0);
+
+  if (number & 0x02)
+    bitSet(NUM_PORT, NUM_BIT_1);
+  else
+    bitClear(NUM_PORT, NUM_BIT_1);
+
+  if (number & 0x04)
+    bitSet(NUM_PORT, NUM_BIT_2);
+  else
+    bitClear(NUM_PORT, NUM_BIT_2);
+
+  if (number & 0x08)
+    bitSet(NUM_PORT, NUM_BIT_3);
+  else
+    bitClear(NUM_PORT, NUM_BIT_3);
+}
+
+void on_tmr() {
+  digit++;
+  if (digit >= 4)
+    digit = 0;
+
+  set_addr(digit);
+  set_number(LED[digit]);
+
+  byte val = BTN_PORT;
+  boolean vbl = val & _BV(BTN_BIT);
+  if (!vbl) {
+    if (btn_was_pressed[digit])
+      btn_pressed[digit] = true;
+    else
+      btn_was_pressed[digit] = true;
+  } else {
+    btn_pressed[digit] = false;
+    btn_was_pressed[digit] = false;
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Timer1.initialize(1000); // in us
+  Timer1.attachInterrupt(on_tmr);
+
+  pinMode(line, INPUT);
+
+  pinMode(dp, OUTPUT);
+
+  pinMode(d0, OUTPUT);
+  pinMode(d1, OUTPUT);
+  pinMode(d2, OUTPUT);
+  pinMode(d3, OUTPUT);
+
+  pinMode(a0, OUTPUT);
+  pinMode(a1, OUTPUT);
+  
+  digitalWrite(dp, LOW);
+
+  digitalWrite(d0, LOW);
+  digitalWrite(d1, LOW);
+  digitalWrite(d2, LOW);
+  digitalWrite(d3, LOW);
+
+  digitalWrite(a0, LOW);
+  digitalWrite(a1, LOW);
+
+  Serial.begin(115200);
+  Serial.println("Start");
+}
+
+void loop() {
+  if (btn_pressed[0])
+    Serial.println("btn 0 pressed");
+  if (btn_pressed[1])
+    Serial.println("btn 1 pressed");
+  if (btn_pressed[2])
+    Serial.println("btn 2 pressed");
+  if (btn_pressed[3])
+    Serial.println("btn 3 pressed");
+}
